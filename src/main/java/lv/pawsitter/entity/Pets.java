2c864379d2febcfce6562f5a5728e39e251eab9c
EBRZ124 <evalds.berzins120@gmail.com>
2026-07-13 12:04:16 +0300

    Added Pets entity and updated User to link the two
    # Conflicts:
    #	src/main/java/lv/pawsitter/entity/User.java

---
+++ src/main/java/lv/pawsitter/entity/Pets.java
@@ -0,0 +1,55 @@
+package lv.pawsitter.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+
+//JPA entity, and Hibernate to map Java fields to a SQL
+@Entity
+@Table(name = "pets")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Pets
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String nickName;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column
+    private String specialNeeds;
+
+    @Column
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void setCreatedAt()
+    {
+        this.createdAt = LocalDateTime.now();
+    }
+}
