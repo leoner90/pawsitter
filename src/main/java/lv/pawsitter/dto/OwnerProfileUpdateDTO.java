8aa1b3bb695a1e50b1f3c009b95bf4d207fcd864
EBRZ124 <evalds.berzins120@gmail.com>
2026-07-14 14:57:51 +0300

    added the ability for a pet owner to add a pet and edit their profile

---
+++ src/main/java/lv/pawsitter/dto/OwnerProfileUpdateDTO.java
@@ -0,0 +1,30 @@
+package lv.pawsitter.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record OwnerProfileUpdateDTO (
+        @NotBlank
+        @Size(min = 2, max = 25)
+        @Pattern(regexp = "^[A-Za-zÀ-ž\\s'-]+$", message = "Must contain only letters")
+        String firstName,
+
+        @NotBlank
+        @Pattern(regexp = "^[A-Za-zÀ-ž\\s'-]+$", message = "Must contain only letters")
+        String lastName,
+
+        @NotBlank
+        @Pattern(regexp = "^\\+?[0-9]{7,15}$", message = "Phone number must be valid international format")
+        String phoneNumber,
+
+        @Size(max = 100)
+        String location,
+
+        @Size(max = 10000)
+        String description,
+
+        String imageUrl
+){
+
+}
