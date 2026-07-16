f2584a43dcad51ffb748892253e4593b28ec36a8
EBRZ124 <evalds.berzins120@gmail.com>
2026-07-16 10:57:51 +0300

    Misspelling fix

--- src/main/java/lv/pawsitter/service/PetServiceImpl.java
+++ src/main/java/lv/pawsitter/service/PetServiceImpl.java
@@ -108,7 +108,7 @@ public class PetServiceImpl implements PetService{
         imageStorageService.deletePetImage(pet.getImageUrl());
         petRepository.delete(pet);
 
-        log.info("Deleted pet with id: {}, for userL {}", petId, email);
+        log.info("Deleted pet with id: {}, for user {}", petId, email);
     }
 
     @Override
