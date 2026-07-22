720da5b5d74f9f275769eeabb3c85f81e4330f7f
Logosorfe <logosorfe@gmail.com>
2026-07-22 16:53:05 +0300

    Should this commit be applied, it will make Zeynep a bit sad.

--- src/test/java/lv/pawsitter/controller/PageControllerUnitTests.java
+++ src/test/java/lv/pawsitter/controller/PageControllerUnitTests.java
@@ -3,6 +3,8 @@ package lv.pawsitter.controller;
 import lv.pawsitter.entity.SitterProfile;
 import lv.pawsitter.entity.User;
 import lv.pawsitter.exception.UserNotFoundException;
+import lv.pawsitter.service.BookingService;
+import lv.pawsitter.service.OwnerProfileService;
 import lv.pawsitter.service.SitterProfileService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +33,12 @@ public class PageControllerUnitTests {
 
     @MockitoBean
     private UserDetailsService userDetailsService;
+
+    @MockitoBean
+    private OwnerProfileService ownerProfileService;
+
+    @MockitoBean
+    private BookingService bookingService;
 
     private SitterProfile buildSitterProfile(Long id) {
         User user = new User();
